dc02087441af09aa0d22b903c6c01b30e71e9d02
sourabh <sumitsrbh@gmail.com>
2024-03-21 16:39:48 +0530

    using only useState to update Input Name

--- code/08 Refs Portals/01-starting-project/src/components/Player.jsx
+++ code/08 Refs Portals/01-starting-project/src/components/Player.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react'
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState('')
+  const [buttonClicked, setButtonClicked] = useState(false)
+  function handlePlayerName(event) {
+    setPlayerName(event.target.value)
+  }
+
+  function handleClick() {
+    setButtonClicked(true)
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {buttonClicked ? playerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handlePlayerName} value={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
-  );
+  )
 }
